243ff5d68db4bdc5b8b57a0de8b1dd200efb17dd
Dionne Stratton <mylife33@hotmail.com>
2020-12-18 00:49:32 -0600

    mvp

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,58 +1,46 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { jwtSecret } = require("../secrets/secrets");
+const AuthModel = require("./auth-model");
+const uniqueName = require("../middleware/uniqueName");
+const { validateUser } = require("../middleware/validateUser");
+const checkUserName = require("../middleware/checkUserName");
 
-router.post('/register', (req, res) => {
-  res.end('implement register, please!');
-  /*
-    IMPLEMENT
-    You are welcome to build additional middlewares to help with the endpoint's functionality.
+router.post("/register", validateUser, uniqueName, (req, res) => {
+  const credentials = req.body;
 
-    1- In order to register a new account the client must provide `username` and `password`:
-      {
-        "username": "Captain Marvel", // must not exist already in the `users` table
-        "password": "foobar"          // needs to be hashed before it's saved
-      }
+  const hash = bcryptjs.hashSync(credentials.password, 10);
+  credentials.password = hash;
 
-    2- On SUCCESSFUL registration,
-      the response body should have `id`, `username` and `password`:
-      {
-        "id": 1,
-        "username": "Captain Marvel",
-        "password": "2a$08$jG.wIGR2S4hxuyWNcBf9MuoC4y0dNy7qC/LbmtuFBSdIhWks2LhpG"
-      }
-
-    3- On FAILED registration due to `username` or `password` missing from the request body,
-      the response body should include a string exactly as follows: "username and password required".
-
-    4- On FAILED registration due to the `username` being taken,
-      the response body should include a string exactly as follows: "username taken".
-  */
+  AuthModel.insert(credentials).then((user) => {
+    res.status(201).json(user);
+  });
 });
 
-router.post('/login', (req, res) => {
-  res.end('implement login, please!');
-  /*
-    IMPLEMENT
-    You are welcome to build additional middlewares to help with the endpoint's functionality.
-
-    1- In order to log into an existing account the client must provide `username` and `password`:
-      {
-        "username": "Captain Marvel",
-        "password": "foobar"
-      }
-
-    2- On SUCCESSFUL login,
-      the response body should have `message` and `token`:
-      {
-        "message": "welcome, Captain Marvel",
-        "token": "eyJhbGciOiJIUzI ... ETC ... vUPjZYDSa46Nwz8"
-      }
-
-    3- On FAILED login due to `username` or `password` missing from the request body,
-      the response body should include a string exactly as follows: "username and password required".
-
-    4- On FAILED login due to `username` not existing in the db, or `password` being incorrect,
-      the response body should include a string exactly as follows: "invalid credentials".
-  */
+router.post("/login", validateUser, checkUserName, (req, res) => {
+  AuthModel.getByUsername(req.body.username).then((user) => {
+    if (bcryptjs.compareSync(req.body.password, user[0].password)) {
+      const token = makeToken(user[0]);
+      res.status(200).json({
+        message: `Welcome ${user.username}`,
+        token,
+      });
+    } else {
+      res.status(401).json("invalid credentials");
+    }
+  });
 });
+
+function makeToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username,
+  };
+  const options = {
+    expiresIn: "1000s",
+  };
+  return jwt.sign(payload, jwtSecret, options);
+}
 
 module.exports = router;
--- api/server.test.js
+++ api/server.test.js
@@ -1,5 +1,81 @@
-// Write your tests here
-test('sanity', () => {
-  expect(2 + 2).toEqual(4)
-  expect(3 + 2).toEqual(5);
-})
+const request = require("supertest");
+const server = require("./server");
+const db = require("../data/dbConfig");
+
+const user = {
+  username: "nunya",
+  password: "bidness",
+};
+const noPassword = {
+  username: "the homies",
+};
+
+beforeAll(async () => {
+  await db.migrate.rollback();
+  await db.migrate.latest();
+});
+
+afterAll(async (done) => {
+  await db.destroy();
+  done();
+});
+
+test("sanity check", () => {
+  expect(true).not.toBe(false);
+});
+
+describe("/register endpoint", () => {
+  beforeEach(async () => {
+    await db("users").truncate();
+  });
+  it("Registers", async () => {
+    const response = await request(server)
+      .post("/api/auth/register")
+      .send(user);
+    expect(response.body).toHaveProperty("id");
+    expect(response.body).toHaveProperty("username");
+    expect(response.body).toHaveProperty("password");
+  });
+  it("Fails with a missing password", async () => {
+    const response = await request(server)
+      .post("/api/auth/register")
+      .send(noPassword);
+    expect(response.body).toBe("username and password required");
+  });
+});
+
+describe("/login endpoint", () => {
+  beforeEach(async () => {
+    await db("users").truncate();
+  });
+  it("Logs in with registered user", async () => {
+    await request(server).post("/api/auth/register").send(user);
+    const response = await request(server).post("/api/auth/login").send(user);
+    expect(response.body).toHaveProperty("message");
+    expect(response.body).toHaveProperty("token");
+  });
+  it("Will not log in an unregistered user", async () => {
+    const response = await request(server).post("/api/auth/login").send(user);
+    expect(response.body).toBe("invalid credentials");
+  });
+});
+
+describe("/jokes endpoint", () => {
+  beforeEach(async () => {
+    await db("users").truncate();
+    await request(server).post("/api/auth/register").send(user);
+  });
+  it("gives 200 status on success", async () => {
+    const {
+      body: { token },
+    } = await request(server).post("/api/auth/login").send(user);
+    const res = await request(server)
+      .get("/api/jokes")
+      .set("Authorization", token);
+    expect(res.status).toBe(200);
+  });
+  it("requires token", async () => {
+    const response = await request(server).get("/api/jokes");
+    expect(response.body).toBe("token required");
+  });
+});
